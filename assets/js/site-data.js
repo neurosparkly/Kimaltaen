07cf46588bbe4a853477b02f44f7eba759b93e7b
neurosparkly <krista.e.ojala@gmail.com>
2026-04-06 13:53:31 +0300

    Add site data with highlights, featured items, and topics

--- assets/js/site-data.js
+++ assets/js/site-data.js
@@ -1,1 +1,65 @@
-
+window.KIMALTAEN_HOME = {
+  highlights: [
+    {
+      title: 'Tunnistamista ja oivalluksia',
+      excerpt: 'Tekstejä hetkistä, joissa oma kokemus alkaa vähitellen saada sanoja.'
+    },
+    {
+      title: 'Turvaa ja ymmärrystä',
+      excerpt: 'Pohdintoja siitä, miten turvallisuus vaikuttaa oloon, yhteyteen ja kohtaamiseen.'
+    },
+    {
+      title: 'Tietoa ja näkökulmia',
+      excerpt: 'Selkeitä tekstejä ilmiöistä, käsitteistä ja tavoista ymmärtää neurodiversiteettiä arvostavasti.'
+    }
+  ],
+  featured: [
+    {
+      title: 'Maskaamisesta näkyväksi',
+      excerpt: 'Miltä tuntuu, kun oma tapa olla alkaa tulla näkyväksi myös itselle.',
+      href: 'maskaamisesta-nakyvaksi.html',
+      image: 'assets/lego-mina.png',
+      imageAlt: 'Lego-hahmo, joka kuvastaa näkyväksi tulemista ja oman itsen tunnistamista.',
+      tag: 'Kokemus'
+    },
+    {
+      title: 'Turvallisuuden merkitys vuorovaikutuksessa',
+      excerpt: 'Yhteys syntyy turvasta, tilasta ja ymmärretyksi tulemisesta.',
+      href: 'turvallisuuden-merkitys-vuorovaikutuksessa.html',
+      image: 'assets/vuorovaikutus.png',
+      imageAlt: 'Kuvitus vuorovaikutuksesta ja turvallisuuden tunteesta.',
+      tag: 'Vuorovaikutus'
+    },
+    {
+      title: 'Erilaisen hermoston vahvuus',
+      excerpt: 'Mitä muuttuu, kun erilaisuutta katsotaan arvostavasti.',
+      href: 'erilaisen-hermoston-vahvuus.html',
+      image: 'assets/hermosto.png',
+      imageAlt: 'Hermostoa kuvaava kuvitus.',
+      tag: 'Neurodiversiteetti'
+    }
+  ],
+  topics: [
+    {
+      title: 'Turva ja vuorovaikutus',
+      excerpt: 'Miten turvallisuus vaikuttaa oloon, yhteyteen ja kohtaamiseen.',
+      href: 'turvallisuuden-merkitys-vuorovaikutuksessa.html',
+      image: 'assets/vuorovaikutus.png',
+      imageAlt: 'Kuvitus turvallisesta vuorovaikutuksesta.'
+    },
+    {
+      title: 'Neurodiversiteetti arjessa',
+      excerpt: 'Ajatuksia erilaisista tavoista kokea, toimia ja olla maailmassa.',
+      href: 'tietoa.html',
+      image: 'assets/hermosto.png',
+      imageAlt: 'Hermostoa kuvaava kuvitus neurodiversiteettiä varten.'
+    },
+    {
+      title: 'Kokemukset ja tunnistaminen',
+      excerpt: 'Kirjoituksia oivalluksista, kuormituksesta, helpotuksesta ja näkyväksi tulemisesta.',
+      href: 'kokemuksia.html',
+      image: 'assets/lego-mina.png',
+      imageAlt: 'Lego-hahmo, joka kuvastaa omaa kokemusta ja tunnistamista.'
+    }
+  ]
+};
